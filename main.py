d85d666acba7e2ea6dff521da5f66a2a82a13963
Hugo Fernandes <hugo.fernandess01@gmail.com>
2019-01-06 15:57:57 -0200

    changing dataset

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 import numpy as np
 import pandas as pd
-
-
-data = pd.read_csv("acidentes.csv", encoding='latin1')
-
-acidentes = data.pop('Qte Acidentes').values
-anos = data.pop('Ano').values
-
-acidentes_anos = np.vstack((acidentes, anos)).T
-
-uf = data.pop('Unidade da Federação').values
-
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(acidentes_anos, uf)
-
-
 from sklearn.neighbors import KNeighborsClassifier
-knn = KNeighborsClassifier(n_neighbors=1)
+
+data = pd.read_csv("pais.csv", encoding='latin1')
+
+valor = data.pop('Qtd Créd. Emit AcInt').values
+anos = data.pop('Ano').values
+pais = data.pop('Países Acordantes').values
+
+valor_anos = np.vstack((valor, anos)).T
+
+X_train, X_test, y_train, y_test = train_test_split(valor_anos, pais)
+
+knn = KNeighborsClassifier(n_neighbors=11)
 knn.fit(X_train, y_train)
 
-test_sample = np.array([[3432, 2012]])
+test_sample = np.array([[195, 2013]])
 
 prediction = knn.predict(test_sample)
 
